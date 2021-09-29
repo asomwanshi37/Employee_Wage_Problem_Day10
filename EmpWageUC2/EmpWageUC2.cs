dc956b56f239eb3ed0a5121e5818e639569eb4c7
asomwanshi37 <asomwanshi37@gmail.co.com>
2021-09-29 13:55:08 +0530

    Calculate Daily Employee Wage

---
+++ EmpWageUC2/EmpWageUC2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWage_UC2_Day9
+{
+    class EmpWageUC2
+    {
+        public static void CalculateDailyEmpWage()
+        {
+            //Constant
+            int IsFullTime = 1;
+            int EMPRATEPERHOUR = 20;
+            // Variables
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            // Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IsFullTime)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMPRATEPERHOUR;
+            Console.WriteLine("Emp Wage :" + empWage);
+        }
+    }
+}
---
+++ EmpWageUC2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWage_UC2_Day9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageUC2.CalculateDailyEmpWage();
+        }
+    }
+}
