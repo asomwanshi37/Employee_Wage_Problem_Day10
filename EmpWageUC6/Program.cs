da3de3fbd086adfa076ad2bdd5b3a5b3cb25abf6
asomwanshi37 <asomwanshi37@gmail.co.com>
2021-09-29 14:09:46 +0530

    Calculate Wages till a condition of total working hours

---
+++ EmpWageUC6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWage_UC6_Day9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageUC6.CalculateWageTill100HrsOr20IsReached();
+        }
+    }
+}
