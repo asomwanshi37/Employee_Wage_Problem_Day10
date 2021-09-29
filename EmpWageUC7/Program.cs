f661c06de2ee872afe11400fac8ce1a254fce9ef
asomwanshi37 <asomwanshi37@gmail.co.com>
2021-09-29 14:18:09 +0530

    Refactor the code to write a Class Method to Compute Emp Wage

---
+++ EmpWageUC7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeeWage_UC7_Day9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageUC7.ComputeEmpWage();
+        }
+    }
+}
