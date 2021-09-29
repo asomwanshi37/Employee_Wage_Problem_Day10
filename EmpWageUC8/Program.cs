37dc6fe8b7ee0364373356f3900140d10a0f5e89
asomwanshi37 <asomwanshi37@gmail.co.com>
2021-09-29 14:21:01 +0530

    Compute Employee Wage for multiple Companies

---
+++ EmpWageUC8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWage_UC8_Day9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageUC8.ComputeEmpWage("tata",8,20,8);
+        }
+    }
+}
