1b384f89e17421268cd0171e870f51a265f65cc6
asomwanshi37 <asomwanshi37@gmail.co.com>
2021-09-29 14:06:51 +0530

    Calculating Wages for a Month

---
+++ EmpWageUC5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWage_UC5_Day9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageUC5.EmpWageUsingSwitch();
+        }
+    }
+}
