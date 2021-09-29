47f0e0f688433b6e22971ed3c47d14891342af99
asomwanshi37 <asomwanshi37@gmail.co.com>
2021-09-29 14:03:21 +0530

    Solving using Switch Case Statement

---
+++ EmpWageUC4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWage_UC4_Day9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageUC4.EmpWageUsingSwitch();
+        }
+    }
+}
