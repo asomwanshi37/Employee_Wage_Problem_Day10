ce90516af68705e950e1998669e131bc7762f44f
asomwanshi37 <asomwanshi37@gmail.co.com>
2021-09-29 13:59:06 +0530

    Add Part Time Employee and Wage

---
+++ EmpWageUC3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWage_UC3_Day9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageUC3.AddPartTimeEmpW();
+        }
+    }
+}
